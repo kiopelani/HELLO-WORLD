464465c717113086f1d0dcde864ade353935d8ca
Kiope Gyzen <kiopelani@gmail.com>
2014-11-28 11:22:24 -0800

    added music controller

--- hello-world/config/routes.rb
+++ hello-world/config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   get 'dashboard' => 'dashboard#index'
 
+  get 'dashboard/music' => 'music#index'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
