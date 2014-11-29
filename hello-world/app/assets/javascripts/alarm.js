5ae5326b93caa68eaef2058f270e914911e6c828
Wesley Gauntt <wesleygauntt92@gmail.com>
2014-11-28 21:46:09 -0800

    currently a huge mess of an alarm, but added moment to asset pipeline so things should get better..

--- hello-world/app/assets/javascripts/alarm.js
+++ hello-world/app/assets/javascripts/alarm.js
@@ -1,25 +1,95 @@
 $(document).ready(function() {
     $('#alarm-button').click(function(e) {
       e.preventDefault();
-      setAlarm($('#alarm-input').val());
+
+      convertAlarmDate($('#alarm-date').val());
+      convertAlarmTime($('#alarm-time').val());
+
+      // convertAlarmTime($('#alarm-input').val());
     });
 });
 
-var setAlarm = function(time){
-  console.log(time);
-  delayedAlert();
-};
 
-function slowAlert() {
-  console.log('in slow alert')
-  alert("That was really slow!");
-}
+  function convertAlarmDate(date){
+    console.log('in converted alarm date')
+    console.log(date)
+  };
 
-function delayedAlert() {
-  console.log('in delayed alert')
-  setTimeout(slowAlert, 2000);
-}
+  function convertAlarmTime(time){
+    console.log('in converted alarm time')
+    console.log(time)
+  };
 
+  function currentDate(){
+    console.log('in current date')
+    var today = new Date().toLocaleString()
+    console.log(today)
+  }
+
+  function currentTime(){
+    console.log('in current time')
+    var rightNow = (new Date()).getTime()
+    console.log(rightNow)
+  }
+
+
+
+
+// function convertCurrentTime(){
+//   var now = new Date();
+//   var ok = (new Date()).getTime()
+  // console.log(ok)
+  // var hour = now.getHours();
+  // var minutes = now.getMinutes();
+  // var timeString = hour + ":" + minutes
+  // console.log('pooop')
+  // console.log(minutes)
+  // if(minutes < 10){
+  //   minutes << "0"
+  // }
+  // console.log(minutes)
+  // var currentConvertedTime = (Number(hour)*60+Number(minutes))*1000
+  // console.log(currentConvertedTime)
+//   return ok
+// }
+
+// var convertAlarmTime = function(time){
+//   var d = new Date(time)
+//   // var alarmTime=(Number(time.split(':')[0])*60+Number(time.split(':')[1]))*1000
+//   console.log('cosajd')
+//   console.log(d)
+//   var alarmTime = d.getTime();
+//   console.log('mcpoop')
+//   console.log(alarmTime)
+//   var currentTime = convertCurrentTime();
+//   // console.log(convertedTime);
+//   // currentTime();
+//   // delayedAlert();
+//   console.log('alarm time')
+//   console.log(alarmTime)
+//   console.log('current time')
+//   console.log(currentTime)
+//   console.log(currentTime - alarmTime);
+//   omg = currentTime - alarmTime
+//   delayedAlert(omg)
+// };
+
+// var setAlarm = function(){
+//   console.log(convertAlarmTime - convertCurrentTime)
+// }
+
+// function slowAlert() {
+//   console.log('in slow alert')
+//   alert("YES");
+// }
+
+// function delayedAlert(milliseconds) {
+//   console.log('in delayed alert')
+//   time = Number(milliseconds)
+//   console.log(milliseconds)
+//   var a = 2000
+//   setTimeout(slowAlert, milliseconds);
+// }
 
 
 
--- hello-world/app/assets/javascripts/application.js
+++ hello-world/app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require turbolinks
 //= require_tree .
 //= require ../../../vendor/assets/bootstrap-3/js/bootstrap.min.js
+//= require ../../../vendor/assets/javascripts/moment.min.js
