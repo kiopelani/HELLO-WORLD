d81540f2dea09849170b4b4a8ff929e33315bb38
Henry <henrywtam@gmail.com>
2014-11-28 19:26:30 -0800

    attempt with react failed

--- hello-world/app/assets/javascripts/application.js
+++ hello-world/app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
 //= require ../../../vendor/assets/bootstrap-3/js/bootstrap.min.js
