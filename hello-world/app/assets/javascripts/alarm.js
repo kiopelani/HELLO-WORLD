6fb63fff01e790252d9890f33ddf2b23093291ca
Wesley Gauntt <wesleygauntt92@gmail.com>
2014-11-28 17:06:28 -0800

    after much turmoil, we can get a popup

--- hello-world/app/assets/javascripts/alarm.js
+++ hello-world/app/assets/javascripts/alarm.js
@@ -1,1 +1,32 @@
-alert('hello!');
+$(document).ready(function() {
+    $('#alarm-button').click(function(e) {
+      e.preventDefault();
+      setAlarm($('#alarm-input').val());
+    });
+});
+
+var setAlarm = function(time){
+  console.log(time);
+  delayedAlert();
+};
+
+function slowAlert() {
+  console.log('in slow alert')
+  alert("That was really slow!");
+}
+
+function delayedAlert() {
+  console.log('in delayed alert')
+  setTimeout(slowAlert, 2000);
+}
+
+
+
+
+// var myVar = setInterval(function(){myTimer()}, 1000);
+
+// function myTimer() {
+//     var d = new Date();
+//     var t = d.toLocaleTimeString();
+//     document.getElementById("demo").innerHTML = t;
+// }
