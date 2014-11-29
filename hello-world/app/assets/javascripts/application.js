dc41c01eee0febd6ec933daafa556d65ada06cdf
Henry <henrywtam@gmail.com>
2014-11-28 16:32:36 -0800

    updated stockapp and reverted back to roobs

--- hello-world/app/assets/javascripts/application.js
+++ hello-world/app/assets/javascripts/application.js
@@ -12,12 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require angular
 //= require turbolinks
 //= require_tree .
 //= require ../../../vendor/assets/bootstrap-3/js/bootstrap.min.js
-//= require stocks/stocks
-
-angular.module('StockApp', [
-  'StockApp.controllers'
-]);
