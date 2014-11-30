81e8e4a035414d0054422031caeabda74e598bc9
Henry <henrywtam@gmail.com>
2014-11-30 14:45:15 -0800

    trying to fix sports/stocks app in dashboard

--- hello-world/app/assets/javascripts/stocks/stocks_controller.js
+++ hello-world/app/assets/javascripts/stocks/stocks_controller.js
@@ -1,12 +1,13 @@
+
 angular.module('StockApp.controllers', [])
 .controller('stockAppCtrl', function($scope, $http, $location) {
   url = "/dashboard/stocks/endpoint"
+  console.log(url)
   $http.get(url).success( function(data) {
     // debugger
     $scope.StockList = (data);
   });
   $scope.add = function(data) {
-    debugger
     $scope.StockList.push()
   }
 });
