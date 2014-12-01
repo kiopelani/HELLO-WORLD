1c68d2a9d4dc058c6791fc7a9745fa6335f14583
Kiope Gyzen <kiopelani@gmail.com>
2014-11-30 23:11:58 -0800

    widgets can be added by dropdown menu

--- hello-world/app/controllers/dashboard_controller.rb
+++ hello-world/app/controllers/dashboard_controller.rb
@@ -3,9 +3,10 @@ class DashboardController < ApplicationController
     @user = current_user
   end
 
-  def add_widgets
-    @user = current_user
-    @widgets = @user.widgets
-    @widget = @user.widgets.new
-  end
+  # def add_widgets
+  #   @user = current_user
+  #   @widgets = @user.widgets
+  #   @widget = @user.widgets.new
+  # end
+
 end
