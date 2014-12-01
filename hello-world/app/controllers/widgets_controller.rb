46d19061b45d72c9b7c0d0a80257c4f3a00c51e6
Youngsun Paik & Kiope Gyzen <pair+boradori.kiopelani@devbootcamp.com>
2014-11-30 16:35:00 -0800

    user can't add widget twice

--- hello-world/app/controllers/widgets_controller.rb
+++ hello-world/app/controllers/widgets_controller.rb
@@ -7,11 +7,23 @@ class WidgetsController < ApplicationController
 
   def create
     @user = current_user
-    @widget = @user.widgets.new(widget_params)
-    if @widget.save
+    user_has_widget = false
+    @user.widgets.each do |widget|
+      if widget.name == widget_params[:name]
+        user_has_widget = true
+        flash[:alert] = "You already have #{widget_params[:name]} widget"
+      end
+    end
+
+    if user_has_widget
       redirect_to dashboard_path
     else
-      @message = params
+      @widget = @user.widgets.new(widget_params)
+      if @widget.save
+        redirect_to dashboard_path
+      else
+        flash[:alert] = "Your widget could not be saved. Please try again."
+      end
     end
   end
 
