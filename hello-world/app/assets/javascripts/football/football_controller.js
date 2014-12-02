0329a47cacc7a6bef1443ecaea3f0dfc9536fed8
Henry <henrywtam@gmail.com>
2014-12-01 17:32:24 -0800

    no one likes debugger

--- hello-world/app/assets/javascripts/football/football_controller.js
+++ hello-world/app/assets/javascripts/football/football_controller.js
@@ -1,6 +1,5 @@
 angular.module('FootballApp.controllers', [])
 .controller('footballAppCtrl', function($scope, $http, footballAPIservice) {
-  debugger
   $scope.gameList = [];
   footballAPIservice.getGames().success(function (data) {
     $scope.GameList = data;
