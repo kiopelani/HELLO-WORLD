e9c6a95c16dfcf888d0cfa0c4f1d7abb9facdc0e
Henry <henrywtam@gmail.com>
2014-12-02 11:48:34 -0800

    issues with oauth + devise

--- hello-world/config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, "b5e01e97cf87437ab7a977d4d326e5af", "6317b109f6f84ba58308cd41179ecc30", scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
-end
