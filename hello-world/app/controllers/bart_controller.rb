45bad856ca84ef3ae48398b759d84572a3f16282
Wesley Gauntt <wesleygauntt92@gmail.com>
2014-12-02 14:01:26 -0800

    creating bart widget dependancies

--- hello-world/app/controllers/bart_controller.rb
+++ hello-world/app/controllers/bart_controller.rb
@@ -2,34 +2,15 @@ class BartController < ApplicationController
   def index
   end
 
-  def tripPlanner(origin, destination, arrival_time)
+  get '/trip/plan' do
+    erb :trip_planner
+  end
 
-    require 'nokogiri'
-    require 'open-uri'
-    require 'httparty'
-
-    xml_file = Nokogiri::XML(open("http://api.bart.gov/api/sched.aspx?cmd=arrive&orig=#{origin}&dest=#{destination}&time=#{arrival_time}&date=now&b=2&a=1&l=1&key=MW9S-E7SL-26DU-VV8V"))
-
-
-    trip_options_xml_information = xml_file.xpath('//schedule/request/trip')
-
-    leg_info = xml_file.at_xpath('//schedule/request/trip/leg')
-
-    train_direction = leg_info.values[10]
-
-    early_trip = trip_options_xml_information[0].values
-
-    on_time_trip = trip_options_xml_information[1].values
-
-    pushing_it_trip = trip_options_xml_information[2].values
-
-    trip_options = []
-    trip_options << early_trip
-    trip_options << on_time_trip
-    trip_options << pushing_it_trip
-    trip_options << train_direction
-
-    return trip_options
+  post '/trip/plan' do
+    @origin = params[:trip].values[0]
+    @destination = params[:trip].values[1]
+    @time = params[:trip].values[2]
+    erb :_your_trip
   end
 
 end
