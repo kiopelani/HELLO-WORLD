38c212ab518215a9a2bf355cd20bea1cbc7da498
Kiope Gyzen <kiopelani@gmail.com>
2014-12-02 12:25:13 -0800

    added dolphins and penguins

--- hello-world/app/models/video.rb
+++ hello-world/app/models/video.rb
@@ -2,7 +2,7 @@ class Video < ActiveRecord::Base
   include HTTParty
 
   def self.search
-  animals = ["sloths", "pandas", "bunnies", "cats", "puppies", "alpacas", "slow+loris", "cute+owl"]
+  animals = ["sloths", "pandas", "bunnies", "cats", "puppies", "alpacas", "slow+loris", "cute+owl", "dolphins", "baby+penguins"]
   animal = animals.sample
   @response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?key=AIzaSyAGNb_uXPZ6TwsKjh2vz_G6G6wiP4HHqUc&part=id&type=video&q=#{animal}&maxResults=1")
     @video_link = @response["items"][0]["id"]["videoId"]
