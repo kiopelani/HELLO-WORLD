1540fdd6a329799ef1d35aa5dd6271ff695f47c6
Henry <henrywtam@gmail.com>
2014-12-02 17:36:36 -0800

    hit wall on gmail api, may come back to

--- hello-world/app/models/user.rb
+++ hello-world/app/models/user.rb
@@ -12,7 +12,8 @@ class User < ActiveRecord::Base
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    user = User.where(:email => data["email"]).first
+    # user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
     if user
       return user
     else
