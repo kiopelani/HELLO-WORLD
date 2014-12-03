ddc5fb2ad468be61e4d0462d84099dd7ad05ba9d
Wesley Gauntt <wesleygauntt92@gmail.com>
2014-12-02 19:09:49 -0800

    can successfully send params for trip

--- hello-world/app/controllers/bart_controller.rb
+++ hello-world/app/controllers/bart_controller.rb
@@ -1,16 +1,22 @@
 class BartController < ApplicationController
+  protect_from_forgery
   def index
   end
 
-  get '/trip/plan' do
+  def new
     erb :trip_planner
   end
 
-  post '/trip/plan' do
+  def create
     @origin = params[:trip].values[0]
     @destination = params[:trip].values[1]
     @time = params[:trip].values[2]
     erb :_your_trip
   end
+
+  private
+    def trip_params
+      params.require(:trip).permit(:origin, :destination, :time)
+    end
 
 end
