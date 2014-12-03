4f80e3ea831cb398714a61dff283093b4bf11629
Wesley Gauntt <wesleygauntt92@gmail.com>
2014-12-03 13:53:05 -0800

    merged with master

--- hello-world/app/controllers/bart_controller.rb
+++ hello-world/app/controllers/bart_controller.rb
@@ -1,8 +1,7 @@
 class BartController < ApplicationController
-  # skip_before_filter :verify_authenticity_token
+
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
-  # protect_from_forgery except: :sign_in
-  # protect_from_forgery
+
   def index
   end
 
@@ -11,12 +10,10 @@ class BartController < ApplicationController
   end
 
   def create
-    p 'I AM HERE' * 500
     @origin = params.values[0]
     @destination = params.values[1]
     @time = params.values[2]
-    p 'I AM ALIVE' * 500
-    render 'trip_info'
+    render '_trip_info'
   end
 
   private
