ad0baae40be681e44e9d1356e17ace99224a501b
Henry Tam <IamHenry@Henrys-MacBook-Air.local>
2014-12-03 09:39:52 -0800

    added joke and wotds widgets

---
+++ hello-world/app/controllers/wotd_controller.rb
@@ -0,0 +1,14 @@
+require 'pp'
+require 'nokogiri'
+require 'open-uri'
+require 'restclient'
+
+class WotdController < ApplicationController
+  def index
+    page = Nokogiri::HTML(RestClient.get("http://dictionary.reference.com/"))
+    @wotd = {}
+    @wotd[:word] = page.css(".wotd").text
+    @wotd[:def] = page.css(".wotd_txt").text
+    pp @wotd
+  end
+end
