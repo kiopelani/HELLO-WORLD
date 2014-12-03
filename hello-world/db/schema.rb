6ff53b2d2f762a2deaf85bc675170f13e82591f6
Kiope Gyzen <kiopelani@gmail.com>
2014-12-02 16:40:32 -0800

    a user with no widgets is prompted to add widgets

--- hello-world/db/schema.rb
+++ hello-world/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130010020) do
+ActiveRecord::Schema.define(version: 20141202182124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
