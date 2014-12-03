50f87d80fd97f794ca71a26333007ccde6fe4bdb
boradori <youngsunpaik@gmail.com>
2014-12-03 12:37:25 -0800

    env added for recipe

--- hello-world/app/models/recipe.rb
+++ hello-world/app/models/recipe.rb
@@ -3,12 +3,9 @@ class Recipe < ActiveRecord::Base
 
   def self.get_recipe
     select_type = ["beef", "chicken", "pork", "potato"].sample
-    @response = HTTParty.get("http://food2fork.com/api/search?key=db47d542c7e6273e08f0b1a248c5cee1&q=shredded%20#{select_type}")
+    api_key = ENV["FOOD2FORK_KEY"]
+    @response = HTTParty.get("http://food2fork.com/api/search?key=#{api_key}&q=shredded%20#{select_type}")
     body = JSON.parse(@response.body)
-    # food = body["recipes"].sample
-    # @title = food["title"]
-    # @f2f_url = food["recipes"]
-    # @image_url = food["image_url"]
   end
 
 
