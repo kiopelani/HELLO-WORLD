72e77c6c584a53458e05a51902f16dc583d42b0f
boradori <youngsunpaik@gmail.com>
2014-12-04 09:58:43 -0800

    added more recipes to display in the begining

--- hello-world/app/models/recipe.rb
+++ hello-world/app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ActiveRecord::Base
   include HTTParty
 
   def self.get_recipe
-    select_type = ["beef", "chicken", "pork", "potato"].sample
+    select_type = ["beef", "chicken", "pork", "potato", "bulgogi", "sushi", "mexican"].sample
     api_key = ENV["FOOD2FORK_KEY"]
     @response = HTTParty.get("http://food2fork.com/api/search?key=#{api_key}&q=#{select_type}")
     body = JSON.parse(@response.body)
